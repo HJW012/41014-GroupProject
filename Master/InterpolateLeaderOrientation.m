64625b81dbda7dde668a9aecf42598500d83b0f4
Liam Thurston <98102534@student.uts.edu.au>
2020-05-28 15:54:05 +1000

    lol

--- Master/InterpolateLeaderOrientation.m
+++ Master/InterpolateLeaderOrientation.m
@@ -1,4 +1,4 @@
-function [desiredOrientation, direction] = InterpolateLeaderOrientation(currentARGlobalPosition, previousARGlobalPosition, tbOrientation, previousOrientation)
+function [desiredOrientation, direction] = InterpolateLeaderOrientation(currentARGlobalPosition, previousARGlobalPosition, tbOrientation, previousOrientation, previousDirection)
     
     distance = sqrt((currentARGlobalPosition.Position.X - previousARGlobalPosition.Position.X)^2 + (currentARGlobalPosition.Position.Y - previousARGlobalPosition.Position.Y)^2 );
     
@@ -9,18 +9,23 @@ function [desiredOrientation, direction] = InterpolateLeaderOrientation(currentA
         leaderOrientation = atan2(diffGlobalPosition(2), diffGlobalPosition(1))
 
         diffOrientation = leaderOrientation - tbOrientation
-         %diffOrientation = leaderOrientation - previousOrientation     
+        %diffOrientation = leaderOrientation - previousOrientation     
 
         if ((-1*(deg2rad(110)) < diffOrientation) && (diffOrientation < deg2rad(110)))
             % Leader driving forward
             direction = 1;
             desiredOrientation = leaderOrientation;
-
+            
+%             if previousDirection == 2
+%                 direction = 1;
+%             else
+%                 direction = previousDirection;
+%             end
+            
         else
             % Leader driving reverse
             direction = -1;
             
-            % Maintain current heading but drive backwards
             if leaderOrientation <= 0
                 desiredOrientation = leaderOrientation + pi;
 
@@ -28,7 +33,12 @@ function [desiredOrientation, direction] = InterpolateLeaderOrientation(currentA
                 desiredOrientation = leaderOrientation - pi;
 
             end
-
+%             if previousDirection == 2
+%                 direction = -1;
+%             else
+%                 direction = -1 * previousDirection;
+%             end
+                        
         end
 
     else
@@ -36,7 +46,5 @@ function [desiredOrientation, direction] = InterpolateLeaderOrientation(currentA
         direction = 2;
         desiredOrientation = previousOrientation;
     end
-
-
 
 end
